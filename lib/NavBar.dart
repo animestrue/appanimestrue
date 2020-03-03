6c78203a7eb5b42adbbe98b7cf52a49c088aaa2b
animestrue <50384384+animestrue@users.noreply.github.com>
2020-03-03 19:49:46 -0300

    Add files via upload

--- lib/NavBar.dart
+++ lib/NavBar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:appanimestrue/constant/Constant.dart';
 import 'package:appanimestrue/curved_navigation_bar.dart';
 import 'package:appanimestrue/screen/GridExplore.dart';
 List<Color> _colors = [
@@ -17,46 +16,56 @@ class _NavBarState extends State<NavBar> {
   int _page = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: CurvedNavigationBar(
-        index: 0,
-        height: 50.0,
-        items: <Widget>[
-          Icon(Icons.home, size: 30),
-          Icon(Icons.video_library, size: 30),
-          Icon(Icons.explore, size: 30),
-          Icon(Icons.calendar_today, size: 30),
-          Icon(Icons.perm_identity, size: 30),
-        ],
-        color: Colors.white,
-        buttonBackgroundColor: Colors.white,
-        backgroundColor: _colors[_page],
-        animationCurve: Curves.easeInOut,
-        animationDuration: Duration(milliseconds: 600),
-        onTap: (int index) {
-          setState(() {
-            _page = index;
 
-          });
-        },
-      ) ,
-      body: bodyColor(context, _page),
+    return  Scaffold(
+
+        bottomNavigationBar: CurvedNavigationBar(
+          index: 0,
+          height: 50.0,
+          items: <Widget>[
+            Icon(Icons.home, size: 30),
+            Icon(Icons.video_library, size: 30),
+            Icon(Icons.explore, size: 30),
+            Icon(Icons.calendar_today, size: 30),
+            Icon(Icons.perm_identity, size: 30),
+          ],
+          color: Colors.white,
+          buttonBackgroundColor: Colors.white,
+          backgroundColor: _colors[_page],
+          animationCurve: Curves.easeInOut,
+          animationDuration: Duration(milliseconds: 600),
+          onTap: (int index) {
+            setState(() {
+              _page = index;
+
+            });
+          },
+        ) ,
+        body:bodyColor(context, _page),
+
     );
+
+
+
   }
 }
 Widget bodyColor(BuildContext context, int _page) {
-
   switch(_page) {
     case 0:
       return  Scaffold(
         appBar: AppBar(
-                    centerTitle: true,
-          title: Text('AnimesTrue'),
-          textTheme: TextTheme(title: TextStyle(color: Colors.black,fontSize: 20)),
-        ),
-        body: new Scaffold(
+          centerTitle: true,
+          elevation: 2.0,
+
+          title: const Text(
+              'Ultimos Episodios', style: TextStyle(color: Colors.white)),
+          brightness: Brightness.dark,
           backgroundColor: _colors[_page],
-        ),
+        ) ,
+        body:  new Scaffold(
+            backgroundColor: _colors[_page],
+            body: GridExplore()
+        ) ,
       );
       break;
     case 1:
@@ -107,5 +116,7 @@ Widget bodyColor(BuildContext context, int _page) {
         ),
       );
       break;
+    default: return null;
+
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:appanimestrue/constant/Constant.dart';
 import 'package:appanimestrue/screen/SplashScreen.dart';
 import 'package:appanimestrue/screen/HomeScreen.dart';
-import 'package:appanimestrue/screen/GridExplore.dart';
+
 
 void main() => runApp(MaterialApp(
   title: 'Animes True',
@@ -16,6 +16,6 @@ void main() => runApp(MaterialApp(
   routes: <String, WidgetBuilder>{
     splash_screen: (BuildContext context) => SplashScreen(),
     home_screen: (BuildContext context) => HomeScreen(),
-    explorer: (BuildContext context) => GridExplore(),
+
   },
 ));
